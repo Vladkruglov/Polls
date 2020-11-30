c9c2efc3aba979b177612f1ccd60d98ac07bf8d5
Vlad Pavlovich Kruglov <67640621+Vladkruglov@users.noreply.github.com>
2020-11-30 20:54:38 +0300

    Migrate

--- polls/models.py
+++ polls/models.py
@@ -21,9 +21,6 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    count = 0
-    if 1+1==3: #Пока что пусть будет так
-        count=+1
-    votes = models.IntegerField(default=count)
+    votes = models.IntegerField(default=0)
     def __str__(self):
         return "{}: {}".format(self.question.question_text, self.choice_text)
