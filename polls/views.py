300af894fa388e9d0f20928335618ad901b21fff
Vlad Pavlovich Kruglov <67640621+Vladkruglov@users.noreply.github.com>
2020-12-02 16:36:18 +0300

    С

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,15 @@
+from polls.polls.models import Question
 from django.shortcuts import render
 from django.http import HttpResponse
-from polls.models import Question
+from . import models
 
 # Create your views here.
 
 def index(request):
-    context={"Questions":Question.objects.all()}
-    return render(request, "questions.html", context)
+    context={"Questions":models.Question.objects.all()}
+    g=models.Question.objects.first()
+    con={"Choices":g.choice_set.all()}
+    return render(request, "questions.html", context, con)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
