c1643c35867ff8b3df3e3f80ba44953b60a594b2
Vlad Pavlovich Kruglov <67640621+Vladkruglov@users.noreply.github.com>
2020-12-20 12:24:50 +0300

    Update romantoint.py

--- polls/romantoint.py
+++ polls/romantoint.py
@@ -1,17 +1,19 @@
 class Solution:
-    def romantoint(o: int):
-        rome_numerals={'M':1000,'MI':999,'MX':900,'D':500,'DI':499,'DX':490,'C':100,'CI':99,'CX':90,'L':50,'LI':49,'LX':40,'X':10,'IX':9,'V':5,'IV':4,'I':1}
-        rome_letters=['M','D','C','L','X','V','I']
+    def int_to_roman(o: int):
+        rome_numerals={'M':1000,'IM':999,'XM':990, 'CM':900,'D':500,'ID':499,'XD':490,'C':100,'IC':99,'XC':90,'L':50,'IL':49,'XL':40,'X':10,'IX':9,'V':5,'IV':4,'I':1}
+        rome_numbers=[rome_letter for rome_letter in rome_numerals]
         s = int(o)
-        for lit in rome_letters:
+        str_list=[]
+        for lit in rome_numbers:
             while s>=rome_numerals[lit]:
-                print(lit)
+                str_list.append(lit)
                 s-=rome_numerals[lit]
                 
+        return ''.join(str_list)
                 
 
                 
                 
 if __name__ == "__main__":
     s = input("")
-    Solution.romantoint(s)
+    print(Solution.int_to_roman(s))
