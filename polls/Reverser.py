8b6df93de5d6c45829e109acb42be502969494f5
Vlad Pavlovich Kruglov <67640621+Vladkruglov@users.noreply.github.com>
2020-12-28 11:46:01 +0300

    new program

---
+++ polls/Reverser.py
@@ -0,0 +1,6 @@
+def reverse_string2(s):
+    return "".join(reversed(s))
+ 
+s = input('what you need to reverse? :')
+data = reverse_string2(s)
+print(data)
---
+++ polls/isValid.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(s: str) -> bool:    
+        dict={')':'(','}':'{',']':'['}
+        stack = []
+        brackets=['(', ')', '{', '}', '[', ']']
+        garbage=[]
+        for i in s:
+            if i not in brackets:
+                garbage.append(i)
+                i=+1
+            elif i not in dict:
+                stack.append(i)
+            elif len(stack) == 0 or stack.pop() != dict[i]:
+                return False
+        if len(stack) == 0:
+            return True
+        else:
+            return False
+        return
+
+                
+if __name__ == "__main__":
+    s = input("string for isValid: ")
+    print(Solution.isValid(s))
